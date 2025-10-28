709e4e4b0cee70885dd9fc1a1dcc692086ac0c25
ricvrdv <costagarcia.r95@gmail.com>
2025-10-28 16:47:41 +0000

    test(ex01): add new test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,9 @@
 #include "iter.hpp"
 
 int main() {
-    
+    int arr[] = {1, 2, 3, 4, 5};
+    std::cout << "Original array: { ";
+    iter(arr, 5, printElement<int>);
+    std::cout << "}\n";
     return 0;
 }
